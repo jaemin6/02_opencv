5f1decb5d9621c7496ca36f4353d9b47f76e7847
jaemin6 <gumt555@gmail.com>
2025-07-29 10:37:08 +0900

    회색 스켈레톤

---
+++ src/bgr2gray.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/like_lenna.png')
+
+img2 = img.astype(np.uint16)
+b, g, r = cv2.split(img2) # 채널별로 분류
+gray1 = ((b + g + r) / 3).astype(np.uint8) # 평균값을 연산 후 dtype 변경
+
+cv2.imshow('original', img)
+cv2.imshow('gray1', gray1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

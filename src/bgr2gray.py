448136fc62e18b631ad78b4f36efddeebbbb37bf
jaemin6 <gumt555@gmail.com>
2025-07-29 10:45:42 +0900

    gray3 3채널(BGR) 값 전부 같음

--- src/bgr2gray.py
+++ src/bgr2gray.py
@@ -6,9 +6,10 @@ img = cv2.imread('../img/like_lenna.png')
 img2 = img.astype(np.uint16)
 b, g, r = cv2.split(img2) # 채널별로 분류
 gray1 = ((b + g + r) / 3).astype(np.uint8) # 평균값을 연산 후 dtype 변경
-
 # 함수 사용법
 gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray3 = cv2.cvtColor(gray2, cv2.COLOR_GRAY2BGR)
+
 
 cv2.imshow('original', img)
 cv2.imshow('gray1', gray1)

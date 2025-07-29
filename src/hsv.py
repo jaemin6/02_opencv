0c2a55fdb42ecb29b4421d5194432d78445b70a1
jaemin6 <gumt555@gmail.com>
2025-07-29 10:53:43 +0900

    hsv방식

--- src/hsv.py
+++ src/hsv.py
@@ -1,14 +1,7 @@
+# BGR을 HSV로 변환 (bgr2hsv.py)
+
 import cv2
 import numpy as np
-
-img = cv2.imread('../img/like_lenna.png')
-
-img2 = img.astype(np.uint16)
-b, g, r = cv2.split(img2) # 채널별로 분류
-gray1 = ((b + g + r) / 3).astype(np.uint8) # 평균값을 연산 후 dtype 변경
-# 함수 사용법
-gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray3 = cv2.cvtColor(gray2, cv2.COLOR_GRAY2BGR)
 
 #---① BGR 컬러 스페이스로 원색 픽셀 생성
 red_bgr = np.array([[[0,0,255]]], dtype=np.uint8)   # 빨강 값만 갖는 픽셀
@@ -22,10 +15,8 @@ green_hsv = cv2.cvtColor(green_bgr, cv2.COLOR_BGR2HSV);
 blue_hsv = cv2.cvtColor(blue_bgr, cv2.COLOR_BGR2HSV);
 yellow_hsv = cv2.cvtColor(yellow_bgr, cv2.COLOR_BGR2HSV);
 
-
-cv2.imshow('original', img)
-cv2.imshow('gray1', gray1)
-cv2.imshow('gray2', gray2)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#---③ HSV로 변환한 픽셀 출력
+print("red:",red_hsv)
+print("green:", green_hsv)
+print("blue", blue_hsv)
+print("yellow", yellow_hsv)

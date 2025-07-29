b4ee2cf4f58111bfd0cc9487ef345e196365cebd
jaemin6 <gumt555@gmail.com>
2025-07-29 11:39:05 +0900

    bitwise로 마스킹하기

---
+++ src/bitwise.py
@@ -0,0 +1,19 @@
+import numpy as np, cv2
+import matplotlib.pylab as plt
+
+# 이미지 읽기
+img = cv2.imread('../img/like_lenna.png')
+
+# 마스크 만들기
+mask = np.zeros_like(img)
+cv2.circle(mask, (260, 210), 100, (255, 255, 255), -1)
+
+# 마스킹
+masked = cv2.bitwise_and(img, mask)
+
+# 결과 출력
+cv2.imshow('original', img)
+cv2.imshow('mask', mask)
+cv2.imshow('masked', masked)
+cv2.waitKey()
+cv2.destroyAllWindows()
